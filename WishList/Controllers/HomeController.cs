16efff5ee6ba87f77c55bbfff8a0fcee5a55491c
josaks <josaks@online.no>
2018-09-11 14:14:17 +0200

    Configure startup

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Controllers
+{
+    public class HomeController
+    {
+		public string index()
+        {
+			return "Hello from controller";
+        }
+    }
+}
