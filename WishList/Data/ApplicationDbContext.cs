c02d10fa6f9d94657c976f68831384551a502560
josaks <josaks@online.no>
2018-09-11 14:49:30 +0200

    Add dbcontext

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.data
+{
+    public class ApplicationDbContext : DbContext
+    {
+		ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Item> items { get; set; }
+    }
+}
